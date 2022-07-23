3b217cb2e9fef0e4c6ed6726d8b77420fba6d17d
Klukov <piotras.kluk@gmail.com>
2022-07-23 11:38:30 +0200

    add default value to ChoosableWrapper

--- src/main/java/org/klukov/utils/combinatorics/choose/ChoosableWrapper.java
+++ src/main/java/org/klukov/utils/combinatorics/choose/ChoosableWrapper.java
@@ -2,11 +2,14 @@ package org.klukov.utils.combinatorics.choose;
 
 import java.math.BigDecimal;
 
-//@Getter
 public record ChoosableWrapper<T>(BigDecimal probabilityCoefficient, T wrappedObject) {
 
     public static <T> ChoosableWrapper<T> of(BigDecimal probabilityCoefficient, T wrappedObject) {
         return new ChoosableWrapper<>(probabilityCoefficient, wrappedObject);
+    }
+
+    public static <T> ChoosableWrapper<T> of(T wrappedObject) {
+        return new ChoosableWrapper<>(BigDecimal.ONE, wrappedObject);
     }
 
     public ChoosableWrapper<T> copy() {
--- src/test/groovy/org/klukov/utils/combinatorics/choose/ChoosableDataGenerator.groovy
+++ src/test/groovy/org/klukov/utils/combinatorics/choose/ChoosableDataGenerator.groovy
@@ -4,7 +4,7 @@ class ChoosableDataGenerator {
 
     static Collection<ChoosableWrapper<String>> sampleStringChoosables() {
         [
-                ChoosableWrapper.of(1 as BigDecimal, "A"),
+                ChoosableWrapper.of("A"),
                 ChoosableWrapper.of(2 as BigDecimal, "B"),
                 ChoosableWrapper.of(3 as BigDecimal, "C"),
                 ChoosableWrapper.of(4 as BigDecimal, "D"),
