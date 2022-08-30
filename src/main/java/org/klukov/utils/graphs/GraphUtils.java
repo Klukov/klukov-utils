ca6a3c4de708456271a21af3e692222cc7c9bb5d
Klukov <piotras.kluk@gmail.com>
2022-08-30 23:58:24 +0200

    make graph utils facade and hide everything behind it

--- src/main/java/org/klukov/utils/graphs/GraphUtils.java
+++ src/main/java/org/klukov/utils/graphs/GraphUtils.java
@@ -1,5 +1,6 @@
 package org.klukov.utils.graphs;
 
+import lombok.experimental.UtilityClass;
 import org.klukov.utils.graphs.common.GraphProcessingException;
 import org.klukov.utils.graphs.parser.ParentGivenGraphFactory;
 import org.klukov.utils.graphs.parser.ParentGivenGraphNodeInput;
@@ -12,9 +13,17 @@ import org.klukov.utils.graphs.relation.GraphRelationFactory;
 import org.klukov.utils.graphs.validation.GraphValidator;
 import org.klukov.utils.graphs.validation.GraphValidatorFactory;
 
+@UtilityClass
 public class GraphUtils {
 
-    private static <ID, T extends ParentGivenGraphNodeInput<ID, T>> ParentGivenGraphParseUseCase<ID, T> generateParentGivenGraphParser() {
+    public <ID, T extends ParentGivenGraphNodeInput<ID, T>> ParentGivenGraphParserResult<ID, T> parseGraphCollection(
+            ParentGivenGraphParseInput<ID, T> parentGivenGraphParseInput
+    ) throws GraphProcessingException {
+        ParentGivenGraphParseUseCase<ID, T> parentGivenGraphParser = generateParentGivenGraphParser();
+        return parentGivenGraphParser.parseGraphCollection(parentGivenGraphParseInput);
+    }
+
+    private <ID, T extends ParentGivenGraphNodeInput<ID, T>> ParentGivenGraphParseUseCase<ID, T> generateParentGivenGraphParser() {
         GraphValidator<ID, T> graphValidator = GraphValidatorFactory.graphValidator();
         DirectionalRelationIdsQuery<ID, T> directionalRelationIdsFinder = GraphRelationFactory.directionalRelationIdsQuery(graphValidator);
         BidirectionalRelationIdsQuery<ID> bidirectionalRelationIdsFinder = GraphRelationFactory.bidirectionalRelationIdsQuery();
@@ -23,12 +32,5 @@ public class GraphUtils {
                 bidirectionalRelationIdsFinder,
                 graphValidator
         );
-    }
-
-    public <ID, T extends ParentGivenGraphNodeInput<ID, T>> ParentGivenGraphParserResult<ID, T> parseGraphCollection(
-            ParentGivenGraphParseInput<ID, T> parentGivenGraphParseInput
-    ) throws GraphProcessingException {
-        ParentGivenGraphParseUseCase<ID, T> parentGivenGraphParser = generateParentGivenGraphParser();
-        return parentGivenGraphParser.parseGraphCollection(parentGivenGraphParseInput);
     }
 }
